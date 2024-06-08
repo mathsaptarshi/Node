487f29c194f7f42a5fae003b9a22d90e230886a4
Saptarshi Biswas <mathsaptarshi@gmail.com>
2024-06-08 13:22:45 +0530

    Code clean up

--- saptarshi/pattern.js
+++ saptarshi/pattern.js
@@ -1,14 +1,16 @@
 function pattern()
 {
+    var str = "";
     for(var i=0;i<5;i++)
     {
         for(var j=0;j<i;j++)
         {
-            console.log('*')
+            str += "*"
         }
-        console.log();
+        str += '\n'
 
     }
+    console.log(str);
 }
 
 pattern();
