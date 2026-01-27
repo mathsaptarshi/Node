6f557aa103c4c9553339103fedd3ac2927b50766
Saptarshi Biswas <mathsaptarshi@gmail.com>
2026-01-28 00:27:37 +0530

    Updated

--- saptarshi/1http_server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var port = 2020;
-http.createServer(function(req,res)
-    {
-        res.end('Hello!!! I am a http server with port number'+port);
-        console.log('Welcome to BWU');
-    }).listen.port;
--- saptarshi/async_file.js
+++ saptarshi/async_file.js
@@ -1,18 +1,17 @@
-var fs=require('fs');
+var fs = require('fs');
 
-var f_read=fs.readFile('test1.txt','utf8',function(err,data){                                                           
-        if(err)
-            console.error(err);
-        else
-        {
-            console.log(f_read);
-            fs.writeFile('text2.txt',data,function(err,data){
-                                            if(err)
-                                                console.error(err);
-                                            else
-                                                console.log('Success!!!');
-            
-                                                            });
+var f_read = fs.readFile('test1.txt', 'utf8', function (err, data) {
+    if (err)
+        console.error(err);
+    else {
+        console.log(f_read);
+        fs.writeFile('text2.txt', data, function (err, data) {
+            if (err)
+                console.error(err);
+            else
+                console.log('Success!!!');
 
-        }
- });
+        });
+
+    }
+});
